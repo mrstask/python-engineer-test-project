189584b23b38d1ac886e6866b8877ccdee7f2783
lazarenko <lazarenko@google.com>
2023-03-17 12:02:22 +0200

    endpoints

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -47,7 +47,66 @@ team_user = db.Table(
     db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
 )
 
+from flask import request
+from flask_restx import Resource, fields, Namespace
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+api = Namespace('teams', description='Team management operations')
+
+team = api.model('Team', {
+    'id': fields.Integer(readOnly=True,
+                         description='The team unique identifier'),
+    'name': fields.String(required=True, description='The team name'),
+    'company_id': fields.Integer(required=True,
+                                 description='The company unique identifier'),
+    'user_ids': fields.List(fields.Integer,
+                            description='List of user unique identifiers')
+})
+
+team = api.model('Team', {
+    'id': fields.Integer(readOnly=True,
+                         description='The team unique identifier'),
+    'name': fields.String(required=True, description='The team name'),
+    'company_id': fields.Integer(required=True,
+                                 description='The company unique identifier'),
+    'user_ids': fields.List(fields.Integer,
+                            description='List of user unique identifiers')
+})
+
+
+@api.route('/')
+class TeamList(Resource):
+    @api.marshal_list_with(team)
+    def get(self):
+        teams = Team.query.all()
+        return teams
+
+    @api.expect(team)
+    @api.marshal_with(team, code=201)
+    def post(self):
+        data = request.json
+        company = Company.query.get(data['company_id'])
+        if not company:
+            return {"message": "Company not found"}, 404
+
+        team = Team(name=data['name'], company=company)
+        db.session.add(team)
+        db.session.flush()
+
+        users = User.query.filter(User.id.in_(data['user_ids'])).all()
+        for user in users:
+            team.users.append(user)
+
+        db.session.commit()
+        return team, 201
+
+
+@api.route('/<int:id>')
+@api.response(404, 'Team not found')
+@api.param('id', 'The team unique identifier')
+class TeamResource(Resource):
+    @api.marshal_with(team)
+    def get(self, id):
+        team = Team.query.get(id)
+        if not team:
+            return {"message": "Team not found"}, 404
+        return team
